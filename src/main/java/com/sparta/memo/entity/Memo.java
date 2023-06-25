3a852e60be1d5b4cbd0238e50afce8226fe25f54
nm0628 <dlaskaaud@naver.com>
2023-06-26 01:48:28 +0900

    휴~~~~~

---
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -0,0 +1,29 @@
+package com.sparta.memo.entity;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor  // 기본생성자 만들어줌
+public class Memo {
+    private Long id;
+    private String username;
+    private String contents;
+
+    public Memo(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+
+
+
+
+    public void update(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+}
