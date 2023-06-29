162edd9f0c0008074f9a2d15e8aa1a3cf192bdd0
nm0628 <dlaskaaud@naver.com>
2023-06-29 11:06:57 +0900

    ..!오류뜨는데 다시한번봐야함

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -1,76 +1,65 @@
 package com.sparta.memo.service;
 
-import com.sparta.memo.dto.MemoRequestDto;
-import com.sparta.memo.dto.MemoResponseDto;
-import com.sparta.memo.entity.Memo;
-import com.sparta.memo.repository.MemoRepository;
-import org.springframework.stereotype.Service;
+        import com.sparta.memo.dto.MemoRequestDto;
+        import com.sparta.memo.dto.MemoResponseDto;
+        import com.sparta.memo.entity.Memo;
+        import com.sparta.memo.repository.MemoRepository;
+        import org.springframework.stereotype.Service;
+        import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+        import java.util.List;
 
 @Service
 public class MemoService {
 
     private final MemoRepository memoRepository;
 
-//    public MemoService(JdbcTemplate jdbcTemplate) {
-//        this.memoRepository = new MemoRepository(jdbcTemplate);
-//    }
-
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
     }
-
-
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         // RequestDto -> Entity
         Memo memo = new Memo(requestDto);
 
         // DB 저장
-       Memo saveMEmo =  memoRepository.save(memo);
+        Memo saveMemo = memoRepository.save(memo);
 
         // Entity -> ResponseDto
-        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+        MemoResponseDto memoResponseDto = new MemoResponseDto(saveMemo);
 
         return memoResponseDto;
     }
 
     public List<MemoResponseDto> getMemos() {
-
         // DB 조회
-        return memoRepository.findAll();
-
+        return memoRepository.findAllByOrderByModifiedAtDesc().stream().map(MemoResponseDto::new).toList();
     }
 
+    @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
-
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if(memo != null) {
-            // memo 내용 수정
-            memoRepository.update(id, requestDto);
+        Memo memo = findMemo(id);
 
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        // memo 내용 수정
+        memo.update(requestDto);
+
+        return id;
     }
-
-
-
 
     public Long deleteMemo(Long id) {
-
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if(memo != null) {
-            // memo 삭제
-            memoRepository.delete(id);
+        Memo memo = findMemo(id);
 
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        // memo 삭제
+        memoRepository.delete(memo);
+
+        return id;
+    }
+
+    private Memo findMemo(Long id) {
+        return memoRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("선택한 메모는 존재하지 않습니다.")
+        );
     }
 }
